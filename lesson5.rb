150f00d97cf14fed662dd3a8225b2256fec1eb6e
shorin48 <126958643+shorin48@users.noreply.github.com>
2023-04-28 14:21:31 +0000

    Initial Commit

---
+++ lesson5.rb
@@ -0,0 +1,9 @@
+total_place = "100"
+ if total_place > "100"
+   puts "みかんを購入。所持金に余りあり。"
+ elsif total_place == "100"
+   puts "みかんを購入。所持金は0円。"
+ else total_place < "100"
+   puts "みかんを購入することができません。"
+
+end
